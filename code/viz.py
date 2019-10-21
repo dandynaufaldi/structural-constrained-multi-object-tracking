ab325bce391948a5164e6c8033202ad77a41d9d5
Dandy Naufaldi <dandy.naufaldi@gmail.com>
2019-10-21 22:19:01 +0700

    calc obj state: position, size, and speed
    Signed-off-by: Dandy Naufaldi <dandy.naufaldi@gmail.com>

--- code/viz.py
+++ code/viz.py
@@ -34,12 +34,13 @@ for scene in scenes:
     gt_path = os.path.join(scene_path, "gt", "gt.csv")
     gt_df = pd.read_csv(gt_path)
 
-
     images_path = os.path.join(scene_path, "img1")
     image_names = os.listdir(images_path)
     image_names.sort()
 
+    obj_states = {}
     for i, image_name in enumerate(image_names):
+        print("=" * 30)
         image_path = os.path.join(images_path, image_name)
         img = cv2.imread(image_path)
         frame_id = i + 1
@@ -49,9 +50,28 @@ for scene in scenes:
             bbox_data = bbox_data.astype("int32")
             bbox_data = bbox_data.tolist()
             for (obj_id, left, top, width, height) in bbox_data:
-                color_idx = obj_id % len(colors)
-                color = colors[color_idx]
-                cv2.rectangle(img, (left, top), (left + width, top + height), color, 2)
+                if obj_id == 1:
+                    color_idx = obj_id % len(colors)
+                    color = colors[color_idx]
+                    cv2.rectangle(img, (left, top), (left + width, top + height), color, 2)
+
+                # object state
+                # [x_mid, y_mid, speed_x, speed_y, width, height, frame_seq]
+                x_mid = left  #+ width // 2
+                y_mid = top  #+ height // 2
+                if obj_id not in obj_states:
+                    obj_states[obj_id] = [x_mid, y_mid, 0, 0, width, height, i]
+                else:
+                    old_x_mid = obj_states[obj_id][0]
+                    old_y_mid = obj_states[obj_id][1]
+                    old_time = obj_states[obj_id][-1]
+                    interval = i - old_time
+                    speed_x = (x_mid - old_x_mid) / interval
+                    speed_y = (y_mid - old_y_mid) / interval
+                    obj_states[obj_id] = [x_mid, y_mid, speed_x, speed_y, width, height, i]
+                print(obj_id, obj_states[obj_id])
+
+        print("=" * 30)
         cv2.imshow(scene, img)
         key = cv2.waitKey(1) & 0xFF
     cv2.destroyAllWindows()
